98cdcaade1e4a41ae11a61869069df1b03038c05
Cassandra Lee <cassichan@Cassandras-MacBook-Air.local>
2022-07-23 15:04:05 -0400

    firebase movies

--- week2.js
+++ week2.js
@@ -1,3 +1,4 @@
+//creating an array of objects
 let students = [
     {
         name: "maxxx",
@@ -9,14 +10,15 @@ let students = [
     },
 ]
 
-function getStudentsNames(studentsArray) {
-    let studentArray = [];
-  for (let i = 0; i < studentsArray.length; i++) {
-   studentArray.push(studentsArray[i].name)
-  }
-  return studentArray;
-}
-console.log(getStudentsNames(students))
+//creating function to return array of strings of student names from an array of students
+// function getStudentsNames(studentsArray) {
+//     let studentArray = [];
+//   for (let i = 0; i < studentsArray.length; i++) {
+//    studentArray.push(studentsArray[i].name)
+//   }
+//   return studentArray;
+// }
+// console.log(getStudentsNames(students))
 
 //Arrow function
 const getStudentsNames = (studentsArray) => {
@@ -26,7 +28,10 @@ const getStudentsNames = (studentsArray) => {
   }
   return studentArray;
 }
-console.log(getStudentsNames(students))
+// console.log(getStudentsNames(students))
+
+//Returns the array of students objects in a table with index. mainly works with objects
+// console.table(students)
 
 
 
